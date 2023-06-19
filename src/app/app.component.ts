1331dc510c07632995063e6b35cdc9bba1836f2e
ASAPJulio <juliosarafyan@gmail.com>
2023-06-19 21:36:14 +0200

    feat: first commit of all files (json, ts, js, css,..)

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'AnnuaireAngular';
+  presentText!: String;
+
+  ngOnInit(): void {
+    this.presentText = "Ceci est un test d'utilisation de Angular";
+  }
+
+  textFonction () {
+  }
+}
---
+++ src/app/identity-card/identity-card.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-identity-card',
+  templateUrl: './identity-card.component.html',
+  styleUrls: ['./identity-card.component.css']
+})
+export class IdentityCardComponent {
+  title = 'AnnuaireAngular';
+}
