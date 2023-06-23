41942bd7540b0e0fd87ab88f614bb12cd6b1bbab
ASAPJulio <juliosarafyan@gmail.com>
2023-06-23 13:45:46 +0200

    test

--- src/app/list-contacts/list-contacts.component.ts
+++ src/app/list-contacts/list-contacts.component.ts
@@ -1,6 +1,6 @@
-import { DatePipe } from '@angular/common';
 import { Component, OnInit} from '@angular/core';
 import { ContactModel } from 'src/contact.model';
+import { ContactService } from 'src/service/contact.service';
 
 @Component({
   selector: 'app-list-contacts',
@@ -11,64 +11,9 @@ export class ListContactsComponent implements OnInit {
 
   contactTab!: ContactModel[];
 
-   ngOnInit(): void {
+  constructor(private tmpContactService: ContactService) { }
 
-    this.contactTab = [
-      {
-        imageInfo: "/assets/img/fournisseur.png",
-        nameInfo: "André",
-        lastNameInfo: "Leroux", 
-        birthdayDate: new Date (1964, 9, 14),
-        sexeInfo: "Masculin",
-        heightInfo: "1m65",
-        typeInfo: "Fournisseur"
-      },
-      {
-        imageInfo: "/assets/img/IMG_3143.JPG",
-        nameInfo: "Jules",
-        lastNameInfo: "Tellier",
-        birthdayDate: new Date (1998, 2, 31),
-        sexeInfo: "Masculin",
-        heightInfo: "1m85",
-        typeInfo: "Client"
-      },
-      {
-        imageInfo: "/assets/img/Liza.jpg",
-        nameInfo: "Liza",
-        lastNameInfo: "Monet",
-        birthdayDate: new Date (1990, 12, 15),
-        sexeInfo: "Féminin",
-        heightInfo: "1m68",
-        typeInfo: "Cliente"
-      },
-      {
-        imageInfo: "/assets/img/Greg.jpg",
-        nameInfo: "Greg",
-        lastNameInfo: "Allo",
-        birthdayDate: new Date (1984, 9, 24) ,
-        sexeInfo: "Masculin",
-        heightInfo: "1m78",
-        typeInfo: "Fournisseur"
-      },
-      {
-        imageInfo: "/assets/img/perso1.png",
-        nameInfo: "Lorie",
-        lastNameInfo: "Gael",
-        birthdayDate: new Date (1996, 6, 9),
-        sexeInfo: "Féminin",
-        heightInfo: "1m70",
-        typeInfo: "Fournisseuse"
-      },
-      {
-        imageInfo: "/assets/img/alien.jpg",
-        nameInfo: "Vorian",
-        lastNameInfo: "Gulgu",
-        birthdayDate: new Date (1710, 8, 17),
-        sexeInfo: "??",
-        heightInfo: "2m05",
-        typeInfo: "Visiteu.r.se"
-      },
-    ]
+  ngOnInit(): void {
+    this.contactTab = this.tmpContactService.getContacts();
   }
-
 }
