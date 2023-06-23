9a2429f2c3df482ee72936897a9684f132a3cafd
ASAPJulio <juliosarafyan@gmail.com>
2023-06-23 12:22:47 +0200

    add: link list-contacts with contact-service by transfer the array on the service

---
+++ src/service/contact.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@angular/core';
+import { ContactModel } from 'src/contact.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ContactService {
+  
+    contactTab: ContactModel[] = [
+      {
+        imageInfo: "/assets/img/fournisseur.png",
+        nameInfo: "André",
+        lastNameInfo: "Leroux",
+        birthdayDate: new Date(1964, 9, 14),
+        sexeInfo: "Masculin",
+        heightInfo: "1m65",
+        typeInfo: "Fournisseur"
+      },
+      {
+        imageInfo: "/assets/img/IMG_3143.JPG",
+        nameInfo: "Jules",
+        lastNameInfo: "Tellier",
+        birthdayDate: new Date(1998, 2, 31),
+        sexeInfo: "Masculin",
+        heightInfo: "1m85",
+        typeInfo: "Client"
+      },
+      {
+        imageInfo: "/assets/img/Liza.jpg",
+        nameInfo: "Liza",
+        lastNameInfo: "Monet",
+        birthdayDate: new Date(1990, 12, 15),
+        sexeInfo: "Féminin",
+        heightInfo: "1m68",
+        typeInfo: "Cliente"
+      },
+      {
+        imageInfo: "/assets/img/Greg.jpg",
+        nameInfo: "Greg",
+        lastNameInfo: "Allo",
+        birthdayDate: new Date(1984, 9, 24),
+        sexeInfo: "Masculin",
+        heightInfo: "1m78",
+        typeInfo: "Fournisseur"
+      },
+      {
+        imageInfo: "/assets/img/perso1.png",
+        nameInfo: "Lorie",
+        lastNameInfo: "Gael",
+        birthdayDate: new Date(1996, 6, 9),
+        sexeInfo: "Féminin",
+        heightInfo: "1m70",
+        typeInfo: "Fournisseuse"
+      },
+      {
+        imageInfo: "/assets/img/alien.jpg",
+        nameInfo: "Vorian",
+        lastNameInfo: "Gulgu",
+        birthdayDate: new Date(1710, 8, 17),
+        sexeInfo: "??",
+        heightInfo: "2m05",
+        typeInfo: "Visiteu.r.se"
+      },
+    ]
+
+    getContacts(): ContactModel[] {
+      return this.contactTab;
+    }
+  
+  };
+
+ 
