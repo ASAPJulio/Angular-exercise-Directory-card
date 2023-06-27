87f94d5a86426636546cb7e5fa0b7bf8a50a9588
ASAPJulio <juliosarafyan@gmail.com>
2023-06-27 14:04:21 +0200

    feat: change routing, transfer app html on identity card to have routing button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DetailsCardComponent } from './details-card/details-card.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ListContactsComponent } from './list-contacts/list-contacts.component';
 
-const routes: Routes = [{path: 'detailsContact/:item', component: DetailsCardComponent}]
+const routes: Routes = [{path: 'detailsContact/:item', component: DetailsCardComponent},
+                        {path: 'cardContact', component: ListContactsComponent}]
 
 @NgModule({
   // declarations: [],
